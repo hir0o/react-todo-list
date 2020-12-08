34d2dc7c394243da38ce993f6da94e7091d27599
mkmk4423 <mk.9.q.kyu@gmail.com>
2020-12-08 12:10:16 +0900

    Add: タスクをstateで管理する

--- src/List.js
+++ src/List.js
@@ -1,16 +1,40 @@
-import { Component } from "react";
+import React from "react";
 
 const ListItem = () => {
   return <li>タスク</li>;
 };
 
-class List extends Component {
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          title: "タイトルです。",
+          isDone: false,
+        },
+        {
+          title: "タイトルです。",
+          isDone: false,
+        },
+        {
+          title: "タイトルです。",
+          isDone: false,
+        },
+        {
+          title: "タイトルです。",
+          isDone: false,
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <ul>
-        <ListItem />
-        <ListItem />
-        <ListItem />
+        {this.state.tasks.map((task) => {
+          return <ListItem>{task.name}</ListItem>;
+        })}
       </ul>
     );
   }
